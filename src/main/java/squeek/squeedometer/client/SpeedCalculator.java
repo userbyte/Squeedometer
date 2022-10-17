f214b5df39a63c79c5937451a5c0b90d379063bb
userbyte <42632711+userbyte@users.noreply.github.com>
2022-10-17 13:37:22 -0400

    change blocks per sec text to m/s

--- src/main/java/squeek/squeedometer/client/SpeedCalculator.java
+++ src/main/java/squeek/squeedometer/client/SpeedCalculator.java
@@ -5,7 +5,7 @@ import squeek.squeedometer.config.SqueedometerConfig;
 public class SpeedCalculator {
     public static String speedText(double speed, SqueedometerConfig.SpeedUnit speedUnit) {
         return switch (speedUnit) {
-            case BLOCKS_PER_SECOND -> String.format("%.2f blocks/sec", metersPerSecond(speed));
+            case BLOCKS_PER_SECOND -> String.format("%.2f m/s", metersPerSecond(speed));
             case KILOMETERS_PER_HOUR -> String.format("%.2f km/h", kilometersPerSecond(speed));
         };
     }
