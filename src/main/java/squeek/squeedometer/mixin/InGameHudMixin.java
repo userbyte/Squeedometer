71174c6acae1a899cf68c6a69c2d279cd68ade08
MFHKiwi <lauri22@online.de>
2023-06-11 19:11:49 +1200

    1.20

--- src/main/java/squeek/squeedometer/mixin/InGameHudMixin.java
+++ src/main/java/squeek/squeedometer/mixin/InGameHudMixin.java
@@ -9,13 +9,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.gui.DrawContext;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
-    @Inject(at = @At("TAIL"), method = "render(Lnet/minecraft/client/util/math/MatrixStack;F)V")
-    private void renderSqueedometerHud(MatrixStack matrices, float tickDelta, CallbackInfo info) {
+    @Inject(at = @At("TAIL"), method = "render(Lnet/minecraft/client/gui/DrawContext;F)V")
+    private void renderSqueedometerHud(DrawContext context, float tickDelta, CallbackInfo info) {
         if (ConfigWrapper.config.enabled)
-            Squeedometer.squeedometerHud.draw(matrices, tickDelta);
+            Squeedometer.squeedometerHud.draw(context, tickDelta);
     }
 }
